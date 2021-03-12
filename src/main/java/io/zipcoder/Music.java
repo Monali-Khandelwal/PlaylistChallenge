1afa1ec170fa7bdf995cf1f2e7de121901fe7999
Monali <khandelwal.monali@gmail.com>
2021-03-11 22:50:32 -0500

    submitting playlist challenge lab

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,3 +1,8 @@
+//An array of n strings, songs, where the value of each element si corresponds to the song
+// at index i in the playlist (where 0 ≤ i < n). An integer, k, the index of song sk.
+// A string, q, the name of the song you wish to switch to.
+// It must return an integer denoting the minimum number of button presses needed to switch from
+// song sk to song q.
 package io.zipcoder;
 
 public class Music {
@@ -9,6 +14,22 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        //String[] songs= new String[playList.length];
+        String startSong = playList[startIndex];
+        //boolean songSelect = true;
+        Integer btnPress = 0;
+        for (int i = startIndex; i < playList.length-1; i++) {
+            if (playList[i] != selection) {
+                btnPress = btnPress+1;
+            } else {
+                return btnPress;
+            }
+
+                if (startSong == playList[0]) {
+                    playList[playList.length - 1].equals(selection);
+                    btnPress = 1;
+                }
+            }
+        return btnPress;
     }
 }
